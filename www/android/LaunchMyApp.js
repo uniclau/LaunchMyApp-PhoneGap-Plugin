901ba9d84e23988352fd857eb60be759acf0dcea
Jordi Moraleda <jmoraleda@users.noreply.github.com>
2014-07-31 14:00:12 +0200

    Android Conditional call to LaunchMyApp Exec
    Uniclau needs to jump to main.html.
    If this code is run twice, the launch URL is not provided as it should.

--- www/android/LaunchMyApp.js
+++ www/android/LaunchMyApp.js
@@ -2,12 +2,15 @@
     "use strict";
     
     function triggerOpenURL() {
+        // Special case for Uniclau => Do not launch on index.html
+        if(location.href.indexOf("main.html") >= 0){
           cordova.exec(
               (typeof handleOpenURL == "function" ? handleOpenURL : null),
               null,
               "LaunchMyApp",
               "checkIntent",
               []);
+        }
     }
 
   document.addEventListener("deviceready", triggerOpenURL, false);
